e9a7cffab8864df012663fd405c62f21e313150f
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-07-18 11:51:59 -0300

    All code refactored, using OOP methodology

--- botoesJS.js
+++ botoesJS.js
@@ -1,120 +1,92 @@
-		//Botões de cima para baixo
-		
-		//1º botão: Valida senha - Toogle na máquina e senha
+const passW = {
+btnTOn : null, //acces html btn
+btnTOff : null, //acces html btn
+btnLogin : null, //acces html btn
+passw : null, //acces html text input
+secMsgMach : null, //acces html display
+msgValid : null, //access html display
 
-		let botaoLigar = document.querySelector('#ligar');
-		let botaoDesligar = document.querySelector('#desligar');
-		let botaoSenha = document.querySelector('#senha');
-		let botaoValidaSenha = document.querySelector('#validaSenha')
+//initialize the buttons and write the cta
+init : () => {
+	passW.btnTOn = document.getElementById('btnTOn');
+	passW.btnTOff = document.getElementById('btnTOff');	
+	passW.btnLogin = document.getElementById('btnLogin')
+	passW.passw = document.getElementById('passw');
+	passW.secMsgMach = document.getElementById('passCode');
+	passW.msgValid = document.getElementById('msgValid');
 
-		botaoSenha.hidden = true;
-		botaoDesligar.hidden = true;
-		botaoValidaSenha.hidden = true;
+	btnTOff.hidden = true;
+	btnLogin.hidden = true;
+	passw.hidden = true;
 
-		let maquinaAtiva = false;
-		let maquinaResult;
+	btnTOn.addEventListener('click', passW.tMachOn);
+	btnTOff.addEventListener('click', passW.tMachOff);
+	btnLogin.addEventListener('click', passW.valid);
 
-		botaoLigar.addEventListener('click', ligaMaquina);
-		botaoDesligar.addEventListener('click', desligaMaquina);
-		botaoValidaSenha.addEventListener('click', validaSenha);
+	paraMachOff = document.createElement('p');
+	paraMachOff.textContent = 'Click to Turn On the machine.';
+	passW.secMsgMach.appendChild(paraMachOff);
+},
 
-		const sectionMsgMaquinas = document.querySelector('.preview');
+//turn on the machine, making the buttons visible and change the text
+tMachOn : () => {
+	btnTOn.hidden = true;
+	btnTOff.hidden = false;
+	passw.hidden = false;
+	passw.focus();
+	btnLogin.hidden = false;
 
-		let paraMaquinaLigada = document.createElement('p');
-		let paraMaquinaDesligada = document.createElement('p');
-		let paraSenhaCorreta = document.createElement('p');
+	paraMachOn = document.createElement('p');
+	paraMachOn.textContent = 'Enter the pasword. Password: jsPractice';
+	paraMachOff.textContent = '';
+	passW.secMsgMach.appendChild(paraMachOn);
+},
 
-		paraMaquinaLigada.textContent = maquinaResult;
-		paraMaquinaDesligada.textContent = 'Clique no botão para ligar a máquina.';
+//turn off the machine, returning to initial status
+tMachOff : () => {
+	btnTOn.hidden = false;
+	btnTOff.hidden = true;
+	btnLogin.hidden = true;
+	passw.hidden = true;
+	passw.value = '';
 
-		(maquinaAtiva === false)?maquinaResult = 'Clique no botão para ligar a máquina.':'';
+	passW.msgValid.innerHTML = '';
+	paraMachOn.textContent = '';
+	paraMachOff.textContent = 'Click to Turn On the machine.';
+},
 
-		//Liga a máquina e ativa os botões de senha e desligar
+//valid if password is correct
+valid : () => {
+	var paraVldOk = '';
+	let vldOk = passw.value;
+	passW.msgValid.innerHTML = '';
 
-		function ligaMaquina () {
-		maquinaAtiva = true;
-		botaoLigar.hidden = true;
-		botaoDesligar.hidden = false;
-		botaoSenha.hidden = false;
-		botaoSenha.focus();
-		botaoValidaSenha.hidden = false;
-		paraMaquinaLigada.textContent = 'Digite a senha para continuar. Senha: praticajs';
-		paraMaquinaDesligada.textContent = '';
-		};
+	//if correct, hide all elements but "turn off button" and login msg
+	if (vldOk == 'jsPractice'){
+		paraVldOk = document.createElement('p');
+		paraVldOk.style.color = 'green';
+		paraVldOk.textContent = 'Successfully logged ln.';
+		paraVldOk.className = 'vldMsg';
+		passW.msgValid.appendChild(paraVldOk);
 
-		//Desativa os botões e reativa o de ligar a máquina
+		paraMachOn.hidden = true;
+		passw.value = '';
+		passw.hidden = true;
+		btnLogin.hidden = true;
 
-		function desligaMaquina () {
-		maquinaAtiva = false;
-		botaoLigar.hidden = false;
-		botaoDesligar.hidden = true;
-		botaoSenha.hidden = true;
-		botaoSenha.value = '';
-		botaoValidaSenha.hidden = true;
-		paraMaquinaLigada.textContent = 'Clique no botão para ligar a máquina.';
-		paraSenhaCorreta.textContent = '';		
-		};
+	//if incorrect, show msg to type the right password
+	} else if(vldOk != '' && vldOk !== 'jsPractice') {
+		var paraVldOk = document.createElement('p');
+		paraVldOk.style.color = 'red';
+		paraVldOk.textContent = 'Enter the correct password.';
+		paraVldOk.className = 'vldMsg';
+		passW.msgValid.appendChild(paraVldOk);
+		passw.value = '';
+	} else {
+		passw.focus();
+	}
+},
 
-		//Valida se a senha está certa ou errada
+};
 
-		function validaSenha () {
-			var valorSenha = botaoSenha.value;
-			if (valorSenha === 'praticajs'){
-				paraSenhaCorreta.textContent = 'Parabéns! Você logou com sucesso.';
-				paraSenhaCorreta.style.color = 'green';
-				botaoSenha.value = '';
-			} else {paraSenhaCorreta.textContent = 'Digite a senha correta.'
-				paraSenhaCorreta.style.color = 'red';
-				botaoSenha.value = '';
-			}
-		};
-
-		sectionMsgMaquinas.appendChild(paraMaquinaDesligada);
-		sectionMsgMaquinas.appendChild(paraMaquinaLigada);
-		sectionMsgMaquinas.appendChild(paraSenhaCorreta);
-
-
-		//2º botão - Pergunta o nome de quem clicou
-
-		let btnClique = document.querySelector('#btn2');
-
-		btnClique.addEventListener('click', quemClicou);
-
-		function quemClicou() {
-			let nome = prompt('Curiooooso né?! Qual seu nome?');
-			alert(`Era só isso mesmo, ${nome}! rsrsrsr`);
-		}
-
-		//Calculadora simples
-
-		let numberLeft = document.querySelector('#number1');
-		let numberRight = document.querySelector('#number2');
-		let basicOper = document.querySelector('#operadoresBasicos');
-		let calc = document.querySelector('#calcular');
-		let parag = document.querySelector('#resultadoCalc');
-
-		calc.addEventListener('click', quatroOperacoes);
-
-		function quatroOperacoes() {
-			if (basicOper.value === '+'){
-				var result = Number(parseFloat(numberLeft.value) + parseFloat(numberRight.value));
-				parag.textContent = `Resultado da soma de ${numberLeft.value} + ${numberRight.value} é ${result}`;
-				numberLeft.value = null;
-				numberRight.value = null;
-			} else if(basicOper.value === '-') {
-				var result = Number(parseFloat(numberLeft.value) - parseFloat(numberRight.value));
-				parag.textContent = `Resultado da subtração de ${numberLeft.value} - ${numberRight.value} é ${result}`;
-				numberLeft.value = null;
-				numberRight.value = null;
-			} else if(basicOper.value === '*') {
-				var result = Number(parseFloat(numberLeft.value) * parseFloat(numberRight.value));
-				parag.textContent = `Resultado da multiplicação de ${numberLeft.value} * ${numberRight.value} é ${result}`;
-				numberLeft.value = null;
-				numberRight.value = null;
-			} else if(basicOper.value === '/') {
-				var result = Number(parseFloat(numberLeft.value) / parseFloat(numberRight.value));
-				parag.textContent = `Resultado da divisão de ${numberLeft.value} / ${numberRight.value} é ${result}`;
-				numberLeft.value = null;
-				numberRight.value = null;
-			} else{parag.textContent = 'Digite um sinal válido: + - * /'}
-		}
+window.addEventListener('load', passW.init);
